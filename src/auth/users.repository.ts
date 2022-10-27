b643f11e9b7efe195c2e656d81c0c2c3b492d8c9
Subhash Chander <subhash.chander@softobiz.com>
2022-10-27 12:31:25 +0530

    my first commit

---
+++ src/auth/users.repository.ts
@@ -0,0 +1,11 @@
+import { EntityRepository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@EntityRepository(User)
+export class UsersRespository extends Respository<User> {
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+    await this.save(user);
+  }
+}
