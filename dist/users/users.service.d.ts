e6deaa71229832ed38ea1b90073df501e68c9161
Subhash Chander <subhash.chander@softobiz.com>
2022-10-27 11:11:34 +0530

    my code

---
+++ dist/users/users.service.d.ts
@@ -0,0 +1,10 @@
+export declare type User = {
+    id: number;
+    name: string;
+    username: string;
+    password: string;
+};
+export declare class UsersService {
+    private readonly users;
+    findOne(username: string): Promise<User | undefined>;
+}
